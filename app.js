855b88e7eb90ea51411dc3cf402868b80de356b2
Monique Rodrigues <psi.souzamonique@gmail.com>
2025-08-04 21:57:41 -0300

    Adiciona funções de Mostrar amigos na tela e Sortear amigos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let amigo [];
+let amigo = [];
 
 function adicionarAmigos() {
     let amigoInput = document.getElementById('amigo');
@@ -16,4 +16,30 @@ function adicionarAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = listaAmigos.join(', ')
     amigoInput.value = '';
+    mostrarAmigosNaTela();
 }
+
+function mostrarAmigosNaTela() {
+    let listaDeAmigosElemento = document.getElementById('listaAmigos');
+    listaDeAmigosElemento.innerHTML = '';
+
+    //Loop para percorrer a lista 'amigos'
+    for (let i = 0; i < amigos.length; i++) {
+        let itemDeLista = document.createElement('li');
+        itemDeLista.textContent = amigos[i];
+        listaDeAmigosElemento.appendChild(itemDeLista);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('Adicione pelo menos 2 amigos para fazer o sorteio!');
+        return
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    let resultadoElenebto = document.getElementById('resultado');
+    resultadoElemento.innerHTML = 'O amigo sorteado foi:' + amigoSorteado;
+
+}
+
